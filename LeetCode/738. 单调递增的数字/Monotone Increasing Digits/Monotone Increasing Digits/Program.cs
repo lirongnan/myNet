e55cd73f315f9c349768c5b12bad352cbe92e564
李荣楠 <260741104@qq.com>
2020-12-16 16:18:56 +0800

    no message

---
+++ LeetCode/738. 单调递增的数字/Monotone Increasing Digits/Monotone Increasing Digits/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monotone_Increasing_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MonotoneIncreasingDigits(332));
+        }
+
+        public static int MonotoneIncreasingDigits(int N)
+        {
+            /*
+             * 抄大佬作业，给大佬跪了，贪心算法哦
+             * 从右到左，每次取两位数字，高位大于低位，则舍掉所有低位后面的数字后，减1
+             */
+            int i = 1;
+            int res = N;
+            while (i <= res / 10)
+            {
+                int n = res / i % 100; // 每次取两个位
+                i *= 10;
+                if (n / 10 > n % 10) // 比较的高一位大于底一位
+                    res = res / i * i - 1; //例如1332 循环第一次变为1330-1=1329 第二次变为1300-1=1299
+            }
+            return res;
+        }
+    }
+}
