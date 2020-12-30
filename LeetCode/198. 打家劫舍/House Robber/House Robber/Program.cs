a609e8fb706bc03513212df62fdbce27b60ff6bd
李荣楠 <260741104@qq.com>
2020-12-30 16:04:24 +0800

    no message

--- LeetCode/198. 打家劫舍/House Robber/House Robber/Program.cs
+++ LeetCode/198. 打家劫舍/House Robber/House Robber/Program.cs
@@ -8,8 +8,34 @@ namespace House_Robber
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] datas)
         {
+
+
+
+        }
+
+        public int GetMedian(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            else if (nums.Length == 1) return nums[0];
+            else if (nums.Length >= 2)
+            {
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    for (int j = 0; j < nums.Length - 1 - i; j++)
+                    {
+                        if (nums[j] > nums[j + 1])
+                        {
+                            int temp = nums[j];
+                            nums[j] = nums[j + 1];
+                            nums[j + 1] = temp;
+                        }
+                    }
+                }
+                return nums[nums.Length / 2];
+            }
+            return 0;
         }
 
         public int Rob(int[] nums)
